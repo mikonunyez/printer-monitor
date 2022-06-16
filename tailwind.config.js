0381bc2f2959c8989b0f7e726628eb764b015fb1
Miko Nunez <nunyezmiko@gmail.com>
2022-06-16 20:27:08 +0800

    added custom fablab bg and red colors to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    colors: {
+      'fablab-bg': '#EAE5E5',
+      'fablab-red': '#751A1B',
+    },
     extend: {},
   },
   plugins: [],
