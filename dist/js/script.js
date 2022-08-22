c5bbb43da869218a053e829d01297b50e6268e45
Miko Nunez <nunyezmiko@gmail.com>
2022-08-22 13:10:36 +0800

    deleted all printers that won't be used for the exhibit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,7 @@
 function getPrinterStatus(printer){
-    // S5
-    const printer_dx = "http://10.10.28.228/api/v1/printer/status"
-
-    // S3
-    const printer_jisi = "http://10.10.28.182/api/v1/printer/status"
-    const printer_dexter = "http://10.10.28.48/api/v1/printer/status"
-    const printer_gee = "http://10.10.28.57/api/v1/printer/status"
     const printer_archie = "http://10.10.141.195/api/v1/printer/status"
-
-    // 3 extended
-    const printer_raphael = "http://10.10.28.219/api/v1/printer/status"
     const printer_michaelangelo = "http://10.10.141.196/api/v1/printer/status"
-    const printer_donatello = "http://10.10.28.154/api/v1/printer/status"
+
 
     const archie_state_id = document.getElementById("printer-archie-state")
     const archie_indicator_id = document.getElementById("printer-archie-indicator")
@@ -21,78 +11,22 @@ function getPrinterStatus(printer){
     const michaelangelo_indicator_id = document.getElementById("printer-michaelangelo-indicator")
     const michaelangelo_status_id = document.getElementById("printer-michaelangelo-status")
 
-    const donatello_state_id = document.getElementById("printer-donatello-state")
-    const donatello_indicator_id = document.getElementById("printer-donatello-indicator")
-    const donatello_status_id = document.getElementById("printer-donatello-status")
 
-    const raphael_state_id = document.getElementById("printer-raphael-state")
-    const raphael_indicator_id = document.getElementById("printer-raphael-indicator")
-    const raphael_status_id = document.getElementById("printer-raphael-status")
 
-    const gee_state_id = document.getElementById("printer-gee-state")
-    const gee_indicator_id = document.getElementById("printer-gee-indicator")
-    const gee_status_id = document.getElementById("printer-gee-status")
-
-    const dx_state_id = document.getElementById("printer-dx-state")
-    const dx_indicator_id = document.getElementById("printer-dx-indicator")
-    const dx_satus_id = document.getElementById("printer-dx-status")
-
-    const jisi_state_id = document.getElementById("printer-jisi-state")
-    const jisi_indicator_id = document.getElementById("printer-jisi-indicator")
-    const jisi_status_id = document.getElementById("printer-jisi-status")
-
-    const dexter_state_id = document.getElementById("printer-dexter-state")
-    const dexter_indicator_id = document.getElementById("printer-dexter-indicator")
-    const dexter_status_id = document.getElementById("printer-dexter-status")
-
-    if (printer == "dx"){
-        var url = printer_dx
-        var printer_state_id = dx_state_id
-        var printer_indicator_id = dx_indicator_id
-        var printer_status_id = dx_satus_id
-    }
-    else if (printer == "jisi"){
-        var url = printer_jisi
-        var printer_state_id = jisi_state_id
-        var printer_indicator_id = jisi_indicator_id
-        var printer_status_id = jisi_status_id
-    }
-    else if (printer == "dexter"){
-        var url = printer_dexter
-        var printer_state_id = dexter_state_id
-        var printer_indicator_id = dexter_indicator_id
-        var printer_status_id = dexter_status_id
-    }
-    else if (printer == "gee"){
-        var url = printer_gee
-        var printer_state_id = gee_state_id
-        var printer_indicator_id = gee_indicator_id
-        var printer_status_id = gee_status_id
-    }
-    else if(printer == "archie"){
+    if(printer == "archie"){
         var url = printer_archie
         var printer_state_id = archie_state_id
         var printer_indicator_id = archie_indicator_id
         var printer_status_id = archie_status_id
     }
-    else if (printer == "raphael"){
-        var url = printer_raphael
-        var printer_state_id = raphael_state_id
-        var printer_indicator_id = raphael_indicator_id
-        var printer_status_id = raphael_status_id
-    }
+
     else if (printer == "michaelangelo"){
         var url = printer_michaelangelo
         var printer_state_id = michaelangelo_state_id
         var printer_indicator_id = michaelangelo_indicator_id
         var printer_status_id = michaelangelo_status_id
     }
-    else if (printer == "donatello"){
-        var url = printer_donatello
-        var printer_state_id = donatello_state_id
-        var printer_indicator_id = donatello_indicator_id
-        var printer_status_id = donatello_status_id
-    }
+
 
     fetch(url)
         .then(response => {
@@ -101,8 +35,7 @@ function getPrinterStatus(printer){
             else {
                 response.json()
                 .then(data => {
-                    let printerStatus = data
-                    
+                    let printerStatus = data                    
                     // changes the online/offline status of the printer
                     printer_state_id.firstChild.nodeValue = "online"
                     printer_indicator_id.classList.remove("text-red-400")
@@ -201,6 +134,7 @@ function getPrintjobInfo(printer){
             else {
                 response.json()
                 .then(data=> {
+
                     let filename = data.name
                     let timeRemaining = data.time_total - data.time_elapsed
                     // console.log(data.result)
