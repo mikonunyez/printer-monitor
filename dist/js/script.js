591b4352ce3b35d1161185d49aaf34a067f11e24
Miko Nunez <nunyezmiko@gmail.com>
2022-10-14 13:05:30 +0800

    made printer IP addresses global to reduce redundancy. Allows potential configuration page for IP addresses as a future feature

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,22 @@
-function getPrinterStatus(printer){
-    // S5
-    const printer_dexter = "http://10.10.28.245/api/v1/printer/status"
+// 
+// GLOBAL VARIABLES FOR PRINTER IP ADDRESSES
+// 
+const printer_dexter_ip = "http://10.10.28.245/" // S5
+
+// S3
+const printer_jisi_ip = "http://10.10.28.235/"
+const printer_dx_ip = "http://10.10.28.128/"
+const printer_gee_ip = "http://10.10.28.167/"
+const printer_archie_ip = "http://10.10.138.35/"
+
+// 3 extended
+const printer_raphael_ip = "http://10.10.28.108/"
+const printer_michaelangelo_ip = "http://10.10.141.196/"
+const printer_donatello_ip = "http://10.10.28.101/"
 
 
-    // S3
-    const printer_jisi = "http://10.10.28.235/api/v1/printer/status"
-    const printer_dx = "http://10.10.28.128/api/v1/printer/status"
-    const printer_gee = "http://10.10.28.167/api/v1/printer/status"
-    const printer_archie = "http://10.10.141.195/api/v1/printer/status"
 
-    // 3 extended
-    const printer_raphael = "http://10.10.28.108/api/v1/printer/status"
-    const printer_michaelangelo = "http://10.10.141.196/api/v1/printer/status"
-    const printer_donatello = "http://10.10.28.101/api/v1/printer/status"
-
+function getPrinterStatus(printer){    
     const archie_state_id = document.getElementById("printer-archie-state")
     const archie_indicator_id = document.getElementById("printer-archie-indicator")
     const archie_status_id = document.getElementById("printer-archie-status")
@@ -21,81 +24,81 @@ function getPrinterStatus(printer){
     const michaelangelo_state_id = document.getElementById("printer-michaelangelo-state")
     const michaelangelo_indicator_id = document.getElementById("printer-michaelangelo-indicator")
     const michaelangelo_status_id = document.getElementById("printer-michaelangelo-status")
-
+    
     const donatello_state_id = document.getElementById("printer-donatello-state")
     const donatello_indicator_id = document.getElementById("printer-donatello-indicator")
     const donatello_status_id = document.getElementById("printer-donatello-status")
-
+    
     const raphael_state_id = document.getElementById("printer-raphael-state")
     const raphael_indicator_id = document.getElementById("printer-raphael-indicator")
     const raphael_status_id = document.getElementById("printer-raphael-status")
-
+    
     const gee_state_id = document.getElementById("printer-gee-state")
     const gee_indicator_id = document.getElementById("printer-gee-indicator")
     const gee_status_id = document.getElementById("printer-gee-status")
-
+    
     const dx_state_id = document.getElementById("printer-dx-state")
     const dx_indicator_id = document.getElementById("printer-dx-indicator")
     const dx_satus_id = document.getElementById("printer-dx-status")
-
+    
     const jisi_state_id = document.getElementById("printer-jisi-state")
     const jisi_indicator_id = document.getElementById("printer-jisi-indicator")
     const jisi_status_id = document.getElementById("printer-jisi-status")
-
+    
     const dexter_state_id = document.getElementById("printer-dexter-state")
     const dexter_indicator_id = document.getElementById("printer-dexter-indicator")
     const dexter_status_id = document.getElementById("printer-dexter-status")
 
     if (printer == "dx"){
-        var url = printer_dx
+        var url = printer_dx_ip 
         var printer_state_id = dx_state_id
         var printer_indicator_id = dx_indicator_id
         var printer_status_id = dx_satus_id
     }
     else if (printer == "jisi"){
-        var url = printer_jisi
+        var url = printer_jisi_ip
         var printer_state_id = jisi_state_id
         var printer_indicator_id = jisi_indicator_id
         var printer_status_id = jisi_status_id
     }
     else if (printer == "dexter"){
-        var url = printer_dexter
+        var url = printer_dexter_ip
         var printer_state_id = dexter_state_id
         var printer_indicator_id = dexter_indicator_id
         var printer_status_id = dexter_status_id
     }
     else if (printer == "gee"){
-        var url = printer_gee
+        var url = printer_gee_ip
         var printer_state_id = gee_state_id
         var printer_indicator_id = gee_indicator_id
         var printer_status_id = gee_status_id
     }
     else if(printer == "archie"){
-        var url = printer_archie
+        var url = printer_archie_ip
         var printer_state_id = archie_state_id
         var printer_indicator_id = archie_indicator_id
         var printer_status_id = archie_status_id
     }
     else if (printer == "raphael"){
-        var url = printer_raphael
+        var url = printer_raphael_ip
         var printer_state_id = raphael_state_id
         var printer_indicator_id = raphael_indicator_id
         var printer_status_id = raphael_status_id
     }
     else if (printer == "michaelangelo"){
-        var url = printer_michaelangelo
+        var url = printer_michaelangelo_ip
         var printer_state_id = michaelangelo_state_id
         var printer_indicator_id = michaelangelo_indicator_id
         var printer_status_id = michaelangelo_status_id
     }
     else if (printer == "donatello"){
-        var url = printer_donatello
+        var url = printer_donatello_ip
         var printer_state_id = donatello_state_id
         var printer_indicator_id = donatello_indicator_id
         var printer_status_id = donatello_status_id
     }
 
-    fetch(url)
+    fetch(url + "api/v1/printer/status")
         .then(response => {
             if (!response.ok){
             }
@@ -124,15 +127,6 @@ function getPrinterStatus(printer){
 }
 
 function getPrintjobInfo(printer){
-    const printer_dx = "http://10.10.28.128/api/v1/print_job"
-    const printer_jisi = "http://10.10.28.235/api/v1/print_job"
-    const printer_dexter = "http://10.10.28.245/api/v1/print_job"
-    const printer_gee = "http://10.10.28.167/api/v1/print_job"
-    const printer_archie = "http://10.10.141.195/api/v1/print_job"
-    const printer_raphael = "http://10.10.28.108/api/v1/print_job"
-    const printer_michaelangelo = "http://10.10.141.196/api/v1/print_job"
-    const printer_donatello = "http://10.10.28.101/api/v1/print_job"
-
     const dx_printjob_id = document.getElementById("printer-dx-printjob")
     const jisi_printjob_id = document.getElementById("printer-jisi-printjob")
     const dexter_printjob_id = document.getElementById("printer-dexter-printjob")
@@ -152,47 +146,47 @@ function getPrintjobInfo(printer){
     const donatello_time_id = document.getElementById("printer-donatello-remainingTime")
 
     if (printer == "dx"){
-        var url = printer_dx
+        var url = printer_dx_ip
         var printjob_id = dx_printjob_id
         var remainingTime_id = dx_time_id
     }
     else if (printer == "jisi"){
-        var url = printer_jisi
+        var url = printer_jisi_ip
         var printjob_id = jisi_printjob_id
         var remainingTime_id = jisi_time_id
     }
     else if (printer == "dexter"){
-        var url = printer_dexter
+        var url = printer_dexter_ip
         var printjob_id = dexter_printjob_id
         var remainingTime_id = dexter_time_id
     }
     else if (printer == "gee"){
-        var url = printer_gee
+        var url = printer_gee_ip
         var printjob_id = gee_printjob_id
         var remainingTime_id = gee_time_id
     }
     else if (printer == "archie"){
-        var url = printer_archie
+        var url = printer_archie_ip
         var printjob_id = archie_printjob_id
         var remainingTime_id = archie_time_id
     }
     else if (printer == "raphael"){
-        var url = printer_raphael
+        var url = printer_raphael_ip
         var printjob_id = raphael_printjob_id
         var remainingTime_id = raphael_time_id
     }
     else if (printer == "michaelangelo"){
-        var url = printer_michaelangelo
+        var url = printer_michaelangelo_ip
         var printjob_id = michaelangelo_printjob_id
         var remainingTime_id = michaelangelo_time_id
     }
     else if (printer == "donatello"){
-        var url = printer_donatello
+        var url = printer_donatello_ip
         var printjob_id = donatello_printjob_id
         var remainingTime_id = donatello_time_id
     }
 
-    fetch(url)
+    fetch(url + "api/v1/print_job")
         .then(response => {
             if (!response.ok){
                 printjob_id.firstChild.nodeValue = " "
